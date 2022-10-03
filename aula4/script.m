8abbad7ed8ee463d9daeccdc96bd3c46f6ff5832
Fernando Ollé <55040050+fernandOlle@users.noreply.github.com>
2022-10-03 10:45:33 -0300

    change to barbara

--- aula4/script.m
+++ aula4/script.m
@@ -1,7 +1,5 @@
 image = imread('barbara_gray.bmp', [;;1]);
 img = corr_gama(image, 1, 10);
 figure, imshow(img, []);
-img = 'mountain.png';
-img = imread(img);
-image = janela(img);
+image = janela('barbara_gray.bmp');
 figure, imshow(img, []);
